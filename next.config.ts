0d331f5666240ba44490399257bfcb3bbd5ad966
pelevanid-lab <pelevanid@gmail.com>
2026-06-29 15:37:22 +0300

    v.9

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,16 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   basePath: '/life',
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/life',
+        basePath: false,
+        permanent: false,
+      },
+    ];
+  },
   /* config options here */
 };
 
